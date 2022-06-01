0e10f5875a0cd27c734999172ea309d872861e09
bazeeel <deakfefe77@gmail.com>
2022-06-01 15:12:16 +0200

    picom changes

--- .local/bin/rpscripts/rp_layout.sh
+++ .local/bin/rpscripts/rp_layout.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-declare options=("layout1
-layout2
-layout3
-layout4
-layout5
+declare options=("full
+vertical
+stack
+horizontal
+grid
 Quit")
 
-choice=$(echo -e "${options[@]}" | dmenu -c -g 1 -l 12 -i -fn 'JetBrains Mono Nerd Font-12' -p 'Choose Layout: ' -nb '#1a1a1a' -sb '#2e8b57')
+choice=$(echo -e "${options[@]}" | dmenu -c -g 1 -l 20 -fn 'JetBrains Mono Nerd Font-12' -p 'Layouts: ' -nb '#1a1a1a' -sb '#2e8b57')
 
 case "$choice" in
     quit)
         echo "program terminated." && exit 1
     ;;
-    layout1)
+    full)
 	    choice="$(ratpoison -c "echo Layout 1" -c "select -" -c "only" -c "next")"
     ;;
-    layout2)
+    vertical)
 	    choice="$(ratpoison -c "echo Layout 2" -c "select -" -c "only" -c "hsplit" -c "next")"
     ;;
-    layout3)
+    stack)
 	    choice="$(ratpoison -c "echo Layout 3" -c "select -" -c "only" -c "hsplit" -c "next" -c "focusright" -c "next" -c "vsplit" -c "next")"
     ;;
-    layout4)
+    horizontal)
 	    choice="$(ratpoison -c "echo Layout 4" -c "select -" -c "only" -c "vsplit" -c "next")"
     ;;
-    layout5)
+    grid)
 	    choice="$(ratpoison -c "echo Layout 5" -c "select -" -c "only" -c "hsplit" -c next -c "vsplit" -c next -c "focusright" -c next -c "vsplit" -c "next")"
 esac
 
